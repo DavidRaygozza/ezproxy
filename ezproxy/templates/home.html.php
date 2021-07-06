21f0f99da7ffa15b3afed44caf7da04ef2f2bf85
David Raygoza <72631829+DavidRaygozza@users.noreply.github.com>
2021-07-05 23:18:28 -0700

    Delete home.html.php

--- ezproxy/templates/home.html.php
+++
@@ -1,9 +0,0 @@
-<h2>Makerspace Logs Database</h2>
-
-<p>Welcome to the Makerspace Logs  Database</p>
-<br>
-<!--
-5/27/20 DavidR NEW 3L: Added a little mroe info to the home page to explain the purpose of the website
--->
-<p>Explore this database of workouts to create a workout plan that will help you reach your fitness goals. Here you can see what workouts other people are doing and use that information to guide you in your journey. You can also search for workouts in specific areas to help you in whicg you may be lacking</p>
-
